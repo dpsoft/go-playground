07120893eb615e5cd88cabb6fbce8464415a343a
Diego <diegolparra@gmail.com>
2022-04-13 14:34:36 -0300

    wip

---
+++ functions/functions.go
@@ -0,0 +1,27 @@
+package functions
+
+func MapSlice[T any, M any](slice []T, f func(T) M) []M {
+	result := make([]M, len(slice))
+	for i, v := range slice {
+		result[i] = f(v)
+	}
+	return result
+}
+
+func Filter[T any](slice []T, f func(T) bool) []T {
+	result := make([]T, 0)
+	for _, v := range slice {
+		if f(v) {
+			result = append(result, v)
+		}
+	}
+	return result
+}
+
+func Reduce[T, M any](s []T, f func(M, T) M, initValue M) M {
+	acc := initValue
+	for _, v := range s {
+		acc = f(acc, v)
+	}
+	return acc
+}
--- main.go
+++ main.go
@@ -2,10 +2,21 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/go-playground/functions"
 	"github.com/go-playground/router"
+	"math"
 )
 
 func main() {
+
+	numbers := []float64{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+
+	slice := functions.MapSlice(numbers, func(n float64) float64 {
+		return math.Pow(n, 2)
+	})
+
+	println(slice)
+
 	r := gin.Default()
 	route := router.NewAdminRoute()
 	route.Register(r.Group("/admin"))
