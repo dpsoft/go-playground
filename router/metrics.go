be3b31fe2de914e6f28cf6451c81af017dc54d43
Diego <diegolparra@gmail.com>
2022-04-13 19:32:57 -0300

    wip

--- router/metrics.go
+++ router/metrics.go
@@ -1,10 +1,13 @@
 package router
 
 import (
+	"fmt"
 	"github.com/bits-and-blooms/bloom/v3"
 	"github.com/gin-gonic/gin"
 	"log"
 	"regexp"
+	"sync/atomic"
+	"time"
 )
 
 type Metric struct {
@@ -17,12 +20,15 @@ type MetricRoute interface {
 }
 
 type metricRoute struct {
-	regex *[]regexp.Regexp
-	bloom *bloom.BloomFilter
+	regex  *[]regexp.Regexp
+	bloom  *bloom.BloomFilter
+	bloom2 atomic.Value
 }
 
 func (m metricRoute) Register(router *gin.RouterGroup) {
 	router.POST("/ingest", m.ingest)
+
+	go m.run()
 }
 
 func (m metricRoute) ingest(ctx *gin.Context) {
@@ -37,18 +43,39 @@ func (m metricRoute) ingest(ctx *gin.Context) {
 	ctx.JSON(200, metric)
 }
 
+func (m metricRoute) run() {
+	ticker := time.NewTicker(5 * time.Second)
+
+	for _ = range ticker.C {
+		_, _ = m.reload()
+	}
+}
+
+func (m metricRoute) reload() (int, error) {
+
+	var bloooom = bloom.NewWithEstimates(100, 0.001)
+	bloooom.AddString("peacha")
+
+	m.bloom2.Swap(bloooom)
+
+	return fmt.Println("Tock")
+}
+
 func NewMetricRoute() MetricRoute {
-	var x []regexp.Regexp
+	var r = []string{"p([a-z]+)ch", "p([a-z]+)ch"}
+	var regexps = make([]regexp.Regexp, len(r))
 
-	r, _ := regexp.Compile("p([a-z]+)ch")
-
-	x = append(x, *r)
+	for i, v := range r {
+		regex, _ := regexp.Compile(v)
+		regexps[i] = *regex
+	}
 
 	var b = bloom.NewWithEstimates(1000000, 0.001)
 	b.AddString("peach")
 
 	return metricRoute{
-		regex: &x,
-		bloom: b,
+		regex:  &regexps,
+		bloom:  b,
+		bloom2: atomic.Value{},
 	}
 }
