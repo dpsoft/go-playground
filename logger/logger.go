3a7695a6d84a64df6e87a726f18dc30689a58d32
Diego <diegolparra@gmail.com>
2022-04-17 12:09:45 -0300

    wip

--- logger/logger.go
+++ logger/logger.go
@@ -1,6 +1,33 @@
 package logger
 
+import (
+	"log"
+	"os"
+)
+
 type Logger interface {
-	Debug(args ...any)
-	Debugf(format string, args ...any)
+	Errorf(format string, args ...interface{})
+	Fatalf(format string, args ...interface{})
+	Fatal(args ...interface{})
+	Infof(format string, args ...interface{})
+	Info(args ...interface{})
+	Warnf(format string, args ...interface{})
+	Debugf(format string, args ...interface{})
+	Debug(args ...interface{})
+}
+
+type BuiltinLogger struct {
+	logger *log.Logger
+}
+
+func newBuiltinLogger(logger *log.Logger) *BuiltinLogger {
+	return &BuiltinLogger{logger: log.New(os.Stdout, "", 5)}
+}
+
+func (l *BuiltinLogger) Debug(args ...any) {
+	l.logger.Println(args...)
+}
+
+func (l *BuiltinLogger) Debugf(format string, args ...any) {
+	l.logger.Printf(format, args...)
 }
